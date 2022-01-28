06dc0a44d56ec15b389c05f464851210d693ddf8
vasanth9 <cheepurupalli.vasanthkumar@gmail.com>
2022-01-28 14:08:23 +0530

    removed navigation

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,61 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router,Routes, Route} from 'react-router-dom';
-import PomodoroClock from '../01-Pomodoro-Clock/PomodoroClock';
-import ColorCycle from '../02-Color-Cycle/ColorCycle';
-import JsonCsv from '../03-JSON2CSV/JsonCsv';
-import QRCodeBadge from '../04-QRCode-Badge/QRCodeBadge';
-import MarkdownPreview from '../05-Markdown-previewer/MarkdownPreview';
-import TypingPractice from '../06-Typing-Practice/TypingPractice';
-import CalculatorCLI from '../07-Calculator-CLI/CalculatorCLI';
-import GithubTimeline from '../08-Github-Timeline/GithubTimeline';
-import BooleBot from '../09-Boole-Bot-Game/BooleBot';
-import SpellIt from '../10-SpellIt/SpellIt';
-import Nav from './Nav';
-
-const HomePage = () =>{
-    return <div>HomePage</div>
-}
+import React, { useState } from "react";
+import HomePage from "./HomePage";
+import PomodoroClock from "../01-Pomodoro-Clock/PomodoroClock";
+import ColorCycle from "../02-Color-Cycle/ColorCycle";
+import JsonCsv from "../03-JSON2CSV/JsonCsv";
+import QRCodeBadge from "../04-QRCode-Badge/QRCodeBadge";
+import MarkdownPreview from "../05-Markdown-previewer/MarkdownPreview";
+import TypingPractice from "../06-Typing-Practice/TypingPractice";
+import CalculatorCLI from "../07-Calculator-CLI/CalculatorCLI";
+import GithubTimeline from "../08-Github-Timeline/GithubTimeline";
+import BooleBot from "../09-Boole-Bot-Game/BooleBot";
+import SpellIt from "../10-SpellIt/SpellIt";
+import Nav from "./Nav";
 
 const Home = () => {
+  const [project, setProject] = useState(0);
+  const projects = [
+    <HomePage />,
+    <PomodoroClock />,
+    <ColorCycle />,
+    <JsonCsv />,
+    <QRCodeBadge />,
+    <MarkdownPreview />,
+    <TypingPractice />,
+    <CalculatorCLI />,
+    <GithubTimeline />,
+    <BooleBot />,
+    <SpellIt />
+  ];
   return (
-    <Router>
-      <Nav />
-      <Routes>
-    
-          <Route exact path="/" element={<HomePage />}></Route>
-          <Route
-            
-            path="/PomodoroClock"
-            element={<PomodoroClock />}
-          ></Route>
-          <Route  path="/ColorCycle" element={<ColorCycle />}></Route>
-          <Route  path="/JsonCsv" element={<JsonCsv />}></Route>
-          <Route  path="/QRCodeBadge" element={<QRCodeBadge />}></Route>
-          <Route
-            
-            path="/markdownpreview"
-            element={<MarkdownPreview />}
-          ></Route>
-          <Route
-            
-            path="/TypingPractice"
-            element={<TypingPractice />}
-          ></Route>
-          <Route
-            
-            path="/CalculatorCLI"
-            element={<CalculatorCLI />}
-          ></Route>
-          <Route
-            
-            path="/GithubTimeline"
-            element={<GithubTimeline />}
-          ></Route>
-          <Route  path="/BooleBot" element={<BooleBot />}></Route>
-          <Route  path="/SpellIt" element={<SpellIt />}></Route>
-          <Route path="*" element={<HomePage />}></Route>
-      </Routes>
-    </Router>
+    <div>
+      <Nav project={project} setProject={setProject} />
+      {projects[project]}
+    </div>
   );
-}
+};
 export default Home;
--- src/Home/Nav.jsx
+++ src/Home/Nav.jsx
@@ -1,54 +1,29 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import './Nav.css';
+import { Row } from "react-bootstrap";
 
-const Nav = () => {
+import "./Nav.css";
+
+const Nav = ({ project,setProject }) => {
+  const style1="nav-link";
+  const style2="nav-link active";
   return (
-    <div className="nav-bar">
-      <NavLink className="nav-link" to="/">
-        Home
-      </NavLink>
-
-      <NavLink className="nav-link" to="/PomodoroClock">
+    <Row className="nav-bar">
+      <span className={project===0?style2:style1} onClick={()=>setProject(0)}>
+        10 Projects
+      </span>
+      <span className={project===1?style2:style1} onClick={()=>setProject(1)}>
         Pomodoro Clock
-      </NavLink>
-
-      <NavLink className="nav-link" to="/ColorCycle">
-        Color Cycle
-      </NavLink>
-
-      <NavLink className="nav-link" to="/JsonCsv">
-        Json Csv{" "}
-      </NavLink>
-
-      <NavLink className="nav-link" to="/QRCodeBadge">
-        QR Code Badge
-      </NavLink>
-
-      <NavLink className="nav-link" to="/markdownpreview">
-        Markdown Preview
-      </NavLink>
-
-      <NavLink className="nav-link" to="/TypingPractice">
-        Typing Practice
-      </NavLink>
-
-      <NavLink className="nav-link" to="/CalculatorCLI">
-        Calculator CLI
-      </NavLink>
-
-      <NavLink className="nav-link" to="/GithubTimeline">
-        Github Timeline
-      </NavLink>
-
-      <NavLink className="nav-link" to="/BooleBot">
-        Boole Bot
-      </NavLink>
-
-      <NavLink className="nav-link" to="/SpellIt">
-        Spell It
-      </NavLink>
-    </div>
+      </span>
+      <span className={project===2?style2:style1} onClick={()=>setProject(2)}>Color Cycle</span>
+      <span className={project===3?style2:style1} onClick={()=>setProject(3)}>Json Csv </span>
+      <span className={project===4?style2:style1} onClick={()=>setProject(4)}>QR Code Badge</span>
+      <span className={project===5?style2:style1} onClick={()=>setProject(5)}>Markdown Preview</span>
+      <span className={project===6?style2:style1} onClick={()=>setProject(6)}>Typing Practice</span>
+      <span className={project===7?style2:style1} onClick={()=>setProject(7)}>Calculator CLI</span>
+      <span className={project===8?style2:style1} onClick={()=>setProject(8)}>Github Timeline</span>
+      <span className={project===9?style2:style1} onClick={()=>setProject(9)}>Boole Bot</span>
+      <span className={project===10?style2:style1} onClick={()=>setProject(10)}>Spell It</span>
+    </Row>
   );
 };
 export default Nav;
