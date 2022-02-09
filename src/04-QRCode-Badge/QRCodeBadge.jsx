b10f16be42529d9b85a38ae8a5dcc26ee8ee716e
vasanth9 <cheepurupalli.vasanthkumar@gmail.com>
2022-02-09 21:12:40 +0530

    Day 11 wrap up

--- src/04-QRCode-Badge/QRCodeBadge.jsx
+++ src/04-QRCode-Badge/QRCodeBadge.jsx
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import qrcode from "qrcode-generator";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Form, Col, Row } from "react-bootstrap";
+import './QRCodeBadge.css';
 
 const QRCodeBadge = () => {
   const [data, setData] = useState({
@@ -10,7 +11,7 @@ const QRCodeBadge = () => {
     Twitter: "Vasanth9110",
     Github: "Vasanth9",
   });
-  
+
   const handleClick = () => {
     var typeNumber = 20;
     var errorCorrectionLevel = "L";
@@ -22,28 +23,60 @@ const QRCodeBadge = () => {
     document.getElementById("placeHolder").innerHTML = qr.createImgTag();
   };
   const handleChange = (field, value) => {
-    setData({...data,[field]:value});
+    setData({ ...data, [field]: value });
+  };
+  const handleCancel = () => {
+    setData({
+    Name: "",
+    Email: "",
+    Twitter: "",
+    Github: "",
+  });
   }
   return (
     <div>
+      <Row>
+        <Col>Name:</Col>
+        <Col>Email:</Col>
+        <Col>Twitter:</Col>
+        <Col>Github:</Col>
+      </Row>
       <Form>
         <Row>
           <Col>
-            <input sm={5} value={data.Name} onChange={(e)=>handleChange("Name",e.target.value)}/>
+            <input
+              className="qrcode-input"
+              value={data.Name}
+              onChange={(e) => handleChange("Name", e.target.value)}
+            />
           </Col>
           <Col>
-            <input value={data.Email} onChange={(e)=>handleChange("Email",e.target.value)}/>
+            <input
+              value={data.Email}
+              className="qrcode-input"
+              onChange={(e) => handleChange("Email", e.target.value)}
+            />
           </Col>
           <Col>
-            <input value={data.Twitter} onChange={(e)=>handleChange("Twitter",e.target.value)}/>
+            <input
+              value={data.Twitter}
+              className="qrcode-input"
+              onChange={(e) => handleChange("Twitter", e.target.value)}
+            />
           </Col>
           <Col>
-            <input value={data.Github} onChange={(e)=>handleChange("Github",e.target.value)}/>
+            <input
+              value={data.Github}
+              className="qrcode-input"
+              onChange={(e) => handleChange("Github", e.target.value)}
+            />
           </Col>
         </Row>
       </Form>
       <button onClick={handleClick}>Create</button>
-      <center id="placeHolder" ></center>
+      <button onClick={handleCancel}>Cancel</button>
+      <center id="Data"></center>
+      <center id="placeHolder"></center>
     </div>
   );
 };
